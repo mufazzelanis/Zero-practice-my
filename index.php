886e3e4bc039ffd64f3f701ef7a71c3ae786d181
Mufazzel Hossain <mufazzelanis@gmail.com>
2024-03-29 15:39:46 +0600

    PHP practice

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+ $name="Mufazzel";
+ $age=28;
+ $hobby= "Cricket";
+ $Father_name="Md. Ashraf Uddin";
+ $mother_name="Hawa Begum";
+ 
+
+ echo "My name is {$name} <br>";
+ echo "I am {$age} old <br>";
+ echo "I like {$hobby} in the world <br>";
+ echo "My Father Name Is {$Father_name}<br>";
+echo "My Mother Name Is {$mother_name}<br>";
+
+?>
